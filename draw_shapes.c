6c278d2ddb5aa80061444df0e482c68caea0c2bc
ZacharyWittmann <zawittmann@miners.utep.edu>
2023-02-01 17:12:55 -0700

    updates done by...

--- draw_shapes.c
+++ draw_shapes.c
@@ -14,7 +14,7 @@ void print_square(int leftCol, int size)
   }
 }
 
-// Prints a triangle of specified height whose left edge is at col leftCol.
+// Prints a triangle of specified height whose left edge is at col leftCol
 void print_triangle(int leftCol, int size)
 {
   for (int row = 0; row <= size; row++) {
@@ -25,4 +25,26 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
+
+// Prints an arrow of specified height whose left edge is at col leftCol
+void print_arrow(int leftCol, int size)
+{
+  for (int row = 0; row <= size; row++) {
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for (col = 0; col < minCol; col++) putchar(' ');
+    for (       ; col <= maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+  int endCol = leftCol + size;
+  int midGap = (size+1)/2;
+  for (int row = 0; row < size; row++){
+    int col;
+    for (col = 0; col < leftCol + midGap; col++) putchar(' ');
+    for (       ; col < endCol + midGap;  col++) putchar('*');
+    putchar('\n');
+  }
+}
+
+  
 
